bd0b9cf4dd0b8e44f1b5fcee330ed934e322ad16
Antonio Gabriel <antoniogabrielti@gmail.com>
2016-04-26 22:55:05 -0300

    Atualizado 26/04 as 22:54

---
+++ Aula1S2B/Codigo1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codigo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x=null;
+            int y=0;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            /*
+            int X = 1; // int é a palavra referenciada, é o apelido do objeto System.Int32
+            System.Int32 y = 1; // System.Int32 é uma Struct(Estrutura)
+            long y = 1;
+            byte b = 200;
+            double d = 1.99;
+            float f = 1.99f; // 1.99 literal ele vai reclamar, tem que converter para float.
+            decimal dc = 1.99m; // 1.99 literal ele vai reclamar, tem que converter para decimal.
+            bool b = true;
+            bool b2 = false;
+            char c = 'a';
+            int x1 = 300;
+            byte bb = (byte)x1;//44*/
+
+        }
+    }
+}
---
+++ Aula1S2B/Codigo1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Codigo1")]
+[assembly: AssemblyDescription("Programa Demo de Console")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Antonio Gabriel Miranda")]
+[assembly: AssemblyProduct("Codigo1")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("MirandaSoft")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55655fbe-ddde-4afd-bd88-cddcfef08617")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
