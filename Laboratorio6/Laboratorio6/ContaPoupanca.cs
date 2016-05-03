05e3b61569137da90c8d84ac8ca8a369bd10b608
S2B DEV MOBILE 2016.1 <S2B DEV MOBILE 2016.1>
2016-05-03 14:55:32 -0300

    Atualizado 03/05 as 14:55

---
+++ Laboratorio6/Laboratorio6/ContaPoupanca.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio6
+{
+    public class ContaPoupanca:Conta
+    {
+        private decimal taxaJuros;
+        private DateTime dataAniversario;
+
+        public ContaPoupanca(decimal j,DateTime d,string t) : base(t)
+        {
+            taxaJuros = j;
+            dataAniversario = d;
+        }
+        public decimal Juros
+        {
+            get { return taxaJuros; }
+            set { taxaJuros = value; }
+        }
+        public DateTime DataAniversario
+        {
+            get { return dataAniversario; }
+        }
+        public void AdicionarRedimento()
+        {
+            DateTime hoje = DateTime.Now;
+            if(hoje.Day==dataAniversario.Day && hoje.Month== dataAniversario.Month)
+            {
+                decimal redimento = this.Saldo * taxaJuros;
+                this.Depositar(redimento);
+            }
+        }
+
+        public override string Id
+        {
+            get { return this.Titular + "(CP)"; }
+        }
+    }
+}
---
+++ Laboratorio6/Laboratorio6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaPoupanca CP = new ContaPoupanca(0.5M, new DateTime(1990,05,03), "joao");
+            CP.Depositar(1000M);
+            Console.WriteLine(CP.DataAniversario);
+            Console.WriteLine(CP.Id);
+            CP.AdicionarRedimento();
+            Console.WriteLine(CP.Saldo);
+            Conta[] ArrayContas = new Conta[5];
+            ArrayContas[0] = CP;
+            Console.WriteLine("---------- Magica Array ---------");
+            Console.WriteLine(ArrayContas[0].Saldo);
+        }
+    }
+}
