cb3a71ebf1d30ef2b5cb5ee5a6583015f8114876
S2B DEV MOBILE 2016.1 <S2B DEV MOBILE 2016.1>
2016-05-03 17:07:51 -0300

    Atualizado 03/05 as 17:07

--- Laboratorio6/Laboratorio7/Program.cs
+++ Laboratorio6/Laboratorio7/Program.cs
@@ -27,6 +27,22 @@ namespace Laboratorio7
             {
                 Console.WriteLine(dispositivo.Estado);
             }
+            TermometroEletrico te = lista[0] as TermometroEletrico; // operador de conversao 
+            Console.WriteLine(te?.Temperatura);// ? <= este operador testa se meu objeto e NULL
+            Console.WriteLine(lista[0] is Lampada); // testa se é daquele tipo(objeto)
+            Console.WriteLine(lista[1] is Lampada);
+            Console.WriteLine(lista[1] is Termometro);
+            Console.WriteLine(lista[1].GetType()); // <= GetType informa o tipo daquele objeto
+
+            string s1 = "abc";
+            string s2 = "aBc";
+
+            Console.WriteLine(s1.Equals(s2, StringComparison.OrdinalIgnoreCase));
+
+            foreach(char c in s1)
+            {
+                Console.WriteLine(c);
+            }
         }
     }
 }
